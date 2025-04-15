4d311a0b5d7fcf0552563bbb4e607b0ff874f6a5
lowiq <loic.aviez91@gmail.com>
2025-04-15 23:35:14 +0200

    feat(app): add sidebar component and update styles to SCSS
    - Integrated <app-sidebar> in app.component.html.
    - Added SidebarComponent to the component imports.
    - Updated style reference from CSS to SCSS.
    - Modified invoke command to use "greet2" instead of "greet".

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { invoke } from "@tauri-apps/api/core";
+import {SidebarComponent} from "./features/components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   greetingMessage = "";
@@ -17,7 +18,7 @@ export class AppComponent {
     event.preventDefault();
 
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    invoke<string>("greet", { name }).then((text) => {
+    invoke<string>("greet2", { name }).then((text) => {
       this.greetingMessage = text;
     });
   }
